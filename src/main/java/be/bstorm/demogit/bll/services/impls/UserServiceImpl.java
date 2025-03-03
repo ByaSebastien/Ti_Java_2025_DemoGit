3bfab38bccffb641fee06de6573651d5ed379772
Bya Sébastien <sebastien.bya@bstorm.be>
2025-03-03 09:14:22 +0100

    getUserById()

---
+++ src/main/java/be/bstorm/demogit/bll/services/impls/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package be.bstorm.demogit.bll.services.impls;
+
+import be.bstorm.demogit.bll.services.UserService;
+import be.bstorm.demogit.dal.repositories.UserRepository;
+import be.bstorm.demogit.dl.entities.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+}
